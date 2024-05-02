b4b7c686b638d329776c5a7381d8b9101bf9304a
Ankulraja <154729128+Ankulraja@users.noreply.github.com>
2024-05-02 23:56:52 +0530

    dash Work almost complete

--- src/Pages/Dash.js
+++ src/Pages/Dash.js
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { Outlet } from "react-router";
-import "./Dash.css"
+import { GoPlusCircle } from "react-icons/go";
+import "./Dash.css";
 const Dash = () => {
   var preDefinString =
     "Ankul Answer The Following Question                          ";
   const [text, setText] = useState("");
   const [preText, setPreText] = useState("");
   const [toggle, setToggle] = useState(false);
-  const [result, setResult]= useState("");
+  const [result, setResult] = useState("");
   const [loader, setLoader] = useState(false);
   const [disable, setDisable] = useState(false);
   const preDefine = (value) => {
@@ -22,25 +23,25 @@ const Dash = () => {
       preDefine(event.target.value);
     }
   };
-  const submitHandler =(event)=>{
-   event.preventDefault();
-   setLoader(true)
-   setTimeout(()=>{
-    var ans="";
-    if(toggle){
-        ans= text.substring(1,text.length);        
-    }else{
-        ans="I Am Not Answering You"
-    }
-    setResult(ans)
-    setLoader(false)
-   },5000)
-   
+  const submitHandler = (event) => {
+    event.preventDefault();
 
-  }
+    setLoader(true);
+    setTimeout(() => {
+      var ans = "";
+      if (toggle) {
+        ans = text.substring(1, text.length);
+      } else {
+        ans = "I Am Not Answering You";
+      }
+      setResult(ans);
+      setDisable(true);
+      setLoader(false);
+    }, 5000);
+  };
   return (
     <div className="w-screen h-screen">
-      <div className="w-full h-72 bg-red-700">
+      <div className="w-full h-72 bg-red-700 relative">
         <div className="w-7/12 mx-auto text-center">
           <div className="w-7/12 mx-auto pt-9 text-center flex justify-center items-center">
             <img
@@ -53,12 +54,20 @@ const Dash = () => {
           <p className="text-white">Welcome To Ankul Answers...</p>
           <p className="pt-10 text-2xl text-gray-200">{result}</p>
         </div>
+        {disable && (
+          <button  onClick={()=>{window.location.reload()}} className="absolute -bottom-7 right-[20%] text-6xl bg-blue-700 rounded-[50%] text-center text-white">
+            <GoPlusCircle></GoPlusCircle>
+          </button>
+        )}
       </div>
-      {loader && (<div className="loader"></div>)}
-      
+      {loader && <div className="loader"></div>}
+
       <div>
         <div className="w-7/12 mx-auto my-14 ">
-          <form onSubmit={submitHandler} className="w-11/12 mx-auto flex flex-col items-center">
+          <form
+            onSubmit={submitHandler}
+            className="w-11/12 mx-auto flex flex-col items-center"
+          >
             <div className="relative w-full">
               <input
                 required
@@ -74,7 +83,7 @@ const Dash = () => {
                 }`}
               ></input>
               {toggle && (
-                <p className="pl-1 absolute top-4 text-xl">{preText}</p>
+                <p className="pl-1 z-10 absolute top-4 text-xl">{preText}</p>
               )}
             </div>
 
@@ -88,15 +97,20 @@ const Dash = () => {
               className={`h-14 w-full text-xl my-5 bg-slate-200 border-b border-gray-500 pl-2 outline-none
              `}
             ></input>
-            <button
-              type="submit"
-              onClick={()=>{setDisable(true)}}
-              className="py-3 px-5 bg-red-700 rounded-lg w-[100px] text-white"
-            >
-              ASK
-            </button>
+            {!disable && (
+              <button
+                type="submit"
+                className="py-3 px-5 bg-red-700 rounded-lg w-[100px] text-white"
+              >
+                ASK
+              </button>
+            )}
           </form>
         </div>
+      </div>
+      <div className="w-7/12 border pb-5 border-gray-300 mx-auto font-horror tracking-widest text-xl text-red-700">
+        <p className="pt-6 px-5 border-t-2 border-red-700">Play at your own risk. We are not responsible for any paranormal activity that may occur</p>
+        <p className="px-5">-By Owner</p>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily:{
+      horror:["Creepster","system-ui"]}
   },
   plugins: [],
 }
