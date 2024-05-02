ceca2d2618683a5d0fc195712557e217c0b68e95
Ankulraja <154729128+Ankulraja@users.noreply.github.com>
2024-05-02 15:31:51 +0530

    Push in Git only

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="w-3 text-red-500">
+      Hello world
+    </div>
+  );
+}
+
+export default App;
